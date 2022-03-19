cf8fa07de7ee01778ec137b02989ce6104dc3f98
grizhe <grizhebusiness@gmail.com>
2022-03-19 15:48:10 -0400

    completed test - got client.py to send information to server.py : implementing to bankSystem now.

--- client.py
+++ client.py
@@ -1,8 +1,25 @@
+import socket
 import hashlib
-server = 'hi'
-print(f"hello, and {server}.")
 
+HEADER = 64
+PORT = 3305
+FORMAT = 'UTF-8'
+DISCONNECET_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(msg)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send('hello')
 
 def hasher(i):
     str(hashlib.sha256(str(i).encode('utf-8')).hexdigest())
